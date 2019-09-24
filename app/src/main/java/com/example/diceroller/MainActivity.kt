ea223d04d719b7c702109e2bb0f8abd4e47ada5e
shihchiang <shihchiang.liu@besi.com>
2019-09-24 13:11:20 +0800

    successfully changed the numbers to dice, onto the next task on the tutorial

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView
+    private var currentDraw: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,23 +19,31 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{rollDice()}
         val countButton: Button = findViewById(R.id.count_button)
         countButton.setOnClickListener{countUp()}
+        diceImage = findViewById(R.id.dice_image)
     }
     private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        val randomInt = Random().nextInt(6) + 1
-        resultText.text = randomInt.toString()
+        currentDraw = Random().nextInt(6) + 1
+        val drawableResource = when(currentDraw){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 
     private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        when (resultText.text) {
-            "Hello World!" -> resultText.text = "1"
-            "6" -> resultText.text = "6"
-            else -> {
-                var resultInt = resultText.text.toString().toInt()
-                resultInt++
-                resultText.text = resultInt.toString()
-            }
+        val drawableResource = when (currentDraw) {
+            0 -> R.drawable.dice_1
+            1 -> R.drawable.dice_2
+            2 -> R.drawable.dice_3
+            3 -> R.drawable.dice_4
+            4 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        currentDraw = currentDraw?.plus(1)
+        diceImage.setImageResource(drawableResource)
     }
 }

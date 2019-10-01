476ea83fc3e08f225f66df1020016fbcba0712d9
shihchiang <shihchiang.liu@besi.com>
2019-10-01 12:47:13 +0800

    last commit for this laptop

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -9,21 +9,22 @@ import android.widget.Toast
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var diceImage: ImageView
-    private var currentDraw: Int = 0
+    lateinit var diceImage_top: ImageView
+    lateinit var diceImage_bottom: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{rollDice()}
-        val countButton: Button = findViewById(R.id.count_button)
-        countButton.setOnClickListener{countUp()}
-        diceImage = findViewById(R.id.dice_image)
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener{clearDice()}
+        diceImage_top = findViewById(R.id.dice_image_top)
+        diceImage_bottom = findViewById(R.id.dice_image_bottom)
     }
-    private fun rollDice(){
-        currentDraw = Random().nextInt(6) + 1
-        val drawableResource = when(currentDraw){
+
+    private fun getRandomDiceImage(): Int{
+        return when(Random().nextInt(6) + 1){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -31,19 +32,16 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage.setImageResource(drawableResource)
     }
 
-    private fun countUp() {
-        val drawableResource = when (currentDraw) {
-            0 -> R.drawable.dice_1
-            1 -> R.drawable.dice_2
-            2 -> R.drawable.dice_3
-            3 -> R.drawable.dice_4
-            4 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
-        }
-        currentDraw = currentDraw?.plus(1)
-        diceImage.setImageResource(drawableResource)
+    private fun rollDice(){
+        diceImage_top.setImageResource(getRandomDiceImage())
+        diceImage_bottom.setImageResource(getRandomDiceImage())
     }
+
+    private fun clearDice(){
+        diceImage_top.setImageResource(R.drawable.empty_dice)
+        diceImage_bottom.setImageResource(R.drawable.empty_dice)
+    }
+
 }
